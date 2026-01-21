d2c4364869791ffbb089492b1c5ce93241d45e8a
abhijeet-178 <abhijeetjha9431@gmail.com>
2026-01-21 14:33:45 +0530

    Refactor AddTransaction component for clarity

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,34 +1,37 @@
+export const dynamic = 'force-dynamic';
 
+import React from 'react';
 import { getUserAccount } from '@/actions/dashboard';
 import { defaultCategories } from '@/data/category';
-import React from 'react'
 import AddTransactionForm from '../_components/transaction-form';
 import { getTransaction } from '@/actions/transaction';
 
-const AddTransaction = async({searchParams}) => {
-    const accounts=await getUserAccount();
+const AddTransaction = async ({ searchParams }) => {
+  const accounts = await getUserAccount();
 
-  const params = await searchParams;
-  const editId = params?.edit;
-   let initialData = null;
+  const editId = searchParams?.edit;
+  let initialData = null;
+
   if (editId) {
     const transaction = await getTransaction(editId);
     initialData = transaction;
   }
+
   return (
-    <div className='max-w-3xl mx-auto px-5'>
-      <h1 className='text-5xl gradient-title mb-8'>{editId?"Edit":"Add"} Transaction</h1>
+    <div className="max-w-3xl mx-auto px-5">
+      <h1 className="text-5xl gradient-title mb-8">
+        {editId ? 'Edit' : 'Add'} Transaction
+      </h1>
+
       <AddTransactionForm
-       accounts={accounts}
+        accounts={accounts}
         categories={defaultCategories}
         editMode={!!editId}
         initialData={initialData}
-      
       />
-
-
     </div>
-  )
-}
+  );
+};
 
-export default AddTransaction
+export default AddTransaction;
+
